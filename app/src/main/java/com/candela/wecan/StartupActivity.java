e6dc9baaf86bf6224ee60aec184849d94a19799e
Amrit Raj <amrit.raj@candelatech.com>
2021-11-05 19:04:06 +0530

    Conflict Resolving try 09

--- app/src/main/java/com/candela/wecan/StartupActivity.java
+++ app/src/main/java/com/candela/wecan/StartupActivity.java
@@ -2,11 +2,19 @@ package com.candela.wecan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import candela.lfresource.lfresource;
 import com.candela.wecan.databinding.ActivityStartupBinding;
 import com.candela.wecan.tests.IPERF;
+import com.candela.wecan.tests.base_tools.LF_Resource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
 
 /**
  * Startup Activity for Candela WE-CAN
@@ -14,23 +22,17 @@ import com.candela.wecan.tests.IPERF;
  */
 public class StartupActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
         getSupportActionBar().hide();
-
-        /**
-         * Switch Activity from here
-         */
-//        TestActivity test = new TestActivity();
-//        IPERF obj = new IPERF(test, "", "", "", "");
+//        LF_Resource p = new LF_Resource(143);
+//        p.start();
 
 
-        Intent myIntent = new Intent(this, navigation.class);
+        Intent myIntent = new Intent(this, ServerConnection.class);
         startActivity(myIntent);
 
     }
 }
-
