8e517de744f72d82e6e917390e259b542f3bb71a
Amrit Raj <amrit.raj@candelatech.com>
2021-11-05 19:01:09 +0530

    Conflict Resolving try 09

--- app/src/main/java/com/candela/wecan/StartupActivity.java
+++ app/src/main/java/com/candela/wecan/StartupActivity.java
@@ -1,15 +1,9 @@
 package com.candela.wecan;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import com.candela.wecan.tests.base_tools.LF_Resource;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
 
 import com.candela.wecan.databinding.ActivityStartupBinding;
 import com.candela.wecan.tests.IPERF;
