32730465a3354383cb195f3ab81b145b40d4c142
Amrit Raj <amrit.raj@candelatech.com>
2021-11-11 11:18:09 +0530

    Enter Page added and working fine

--- app/src/main/java/com/candela/wecan/StartupActivity.java
+++ app/src/main/java/com/candela/wecan/StartupActivity.java
@@ -2,21 +2,14 @@ package com.candela.wecan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.os.Handler;
 
 //import candela.lfresource.lfresource;
-import com.candela.wecan.databinding.ActivityStartupBinding;
-import com.candela.wecan.tests.IPERF;
 //import com.candela.wecan.tests.base_tools.LF_Resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
 
 /**
  * Startup Activity for Candela WE-CAN
@@ -30,7 +23,7 @@ public class StartupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
         getSupportActionBar().hide();
-        button = (Button) findViewById(R.id.button3);
+        button = (Button) findViewById(R.id.enter_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +39,7 @@ public class StartupActivity extends AppCompatActivity {
 
         public void openServerConnection () {
 
-            Intent myIntent = new Intent(this, ServerConnection.class);
+            Intent myIntent = new Intent(this, navigation.class);
             startActivity(myIntent);
 
 
--- app/src/main/java/com/candela/wecan/ui/home/HomeFragment.java
+++ app/src/main/java/com/candela/wecan/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.candela.wecan.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,15 +17,22 @@ import androidx.lifecycle.ViewModelProvider;
 import com.candela.wecan.R;
 import com.candela.wecan.databinding.FragmentHomeBinding;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private Button refresh_button;
+    private static final String FILE_NAME = "data.conf";
+    private TextView ip_show;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -32,7 +41,32 @@ public class HomeFragment extends Fragment {
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+//                refresh_button = getView().findViewById(R.id.refresh);
+                ip_show = getView().findViewById(R.id.ip_show);
+
+                FileInputStream fis = null;
+                try {
+                    fis = getActivity().openFileInput(FILE_NAME);
+                    InputStreamReader isr = new InputStreamReader(fis);
+                    BufferedReader br = new BufferedReader(isr);
+                    StringBuilder sb = new StringBuilder();
+                    String ip;
+
+                    ip= br.readLine();
+                    ip_show.setText(ip);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (fis != null){
+                        try {
+                            fis.close();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
         });
         return root;
