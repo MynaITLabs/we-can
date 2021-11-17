ebc8a77d516d73449f869730a590d7018c1dcbc8
Amrit Raj <amrit.raj@candelatech.com>
2021-11-17 21:39:26 +0530

    Condition checking for resource id

--- app/src/main/java/com/candela/wecan/StartupActivity.java
+++ app/src/main/java/com/candela/wecan/StartupActivity.java
@@ -2,7 +2,6 @@ package com.candela.wecan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,8 +22,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-import com.candela.wecan.tests.base_tools.file_handler;
-import com.candela.wecan.tests.base_tools.file_handler.*;
 //import candela.lfresource.lfresource;
 //import com.candela.wecan.tests.base_tools.LF_Resource;
 
@@ -42,10 +39,10 @@ public class StartupActivity extends AppCompatActivity {
     private static final String FILE_NAME = "data.conf";
     private TextView server_ip;
     static int state;
-    //    private Boolean server_connected_status = false;
-    private String ip, ssid, passwd;
+//    private Boolean server_connected_status = false;
+    private String ip, ssid, passwd, resource_id;
+    public String new_resource_id;
 
-    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,69 +52,87 @@ public class StartupActivity extends AppCompatActivity {
         server_ip = findViewById(R.id.ip_enter_page);
 //        LF_Resource p = new LF_Resource(143, "192.168.52.100", "2");
 //        p.start();
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            ip= br.readLine();
+            ssid = br.readLine();
+            passwd = br.readLine();
+            resource_id = br.readLine();
 
-        file_handler file_obj = new file_handler(getApplicationContext());
-//        file_obj.set_val("Server", "192.168.200.15");
-        ip = file_obj.get_val("SALE_PRODUCTS");
-        server_ip.setText(ip);
-        Log.d("SALE_PRODUCTS: ", ""+ ip);
+            server_ip.setText(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
 
         button.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("NewApi")
             @Override
             public void onClick(View v) {
-                file_handler file_obj = new file_handler(getApplicationContext());
-                file_obj.set_val("Key", "Value");
-                String txt = file_obj.get_val("SALES");
 
-                System.out.println("Data SALES "+txt);
-                String ip = server_ip.getText().toString().trim();
-                String data = ip + "\n" + ssid + "\n" + passwd;
-                if( ip.length() == 0 )
+//                String resource_code = "-1";
+                String new_resource_id = resource_id;
+
+                String new_ip = server_ip.getText().toString().trim();
+                if( new_ip.length() == 0 )
                     server_ip.setError( "IP is required!" );
                 else{
-//                    Toast.makeText(v.getContext(), "Configuration Saved Successfully ", Toast.LENGTH_SHORT).show();
-                file_handler file_obj1 = new file_handler(getApplicationContext());
-//        file_obj.set_val("Server", "192.168.200.15");
-                ip = file_obj1.get_val("SALE_PRODUCTS");
-                server_ip.setText(ip);
+                    if (ip == null){
+                        resource_id = "-1";
+                        connect_server(new_ip, resource_id, v);
+                    }
+                    else if(ip == new_ip){
+                        if (resource_id == null){
+                            resource_id = "-1";
+                            connect_server(new_ip, resource_id, v);
 
+                        }else{
+                            resource_id = resource_id;
+                            connect_server(new_ip, resource_id, v);
+                        }
+                    }
+                    else if (!(ip.equals(new_ip))){
+                        resource_id = "-1";
+                        connect_server(new_ip, resource_id, v);
+                        Log.d("onClick: ", "IP NOT EQUAL");
+                    }
+                    String data = new_ip + "\n" + ssid + "\n" + passwd + "\n" + new_resource_id;
+//                    server_connected_status = true;
+                    FileOutputStream fos = null;
+                    try {
+                        fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+                        fos.write(data.getBytes(StandardCharsets.UTF_8));
+//                        Toast.makeText(v.getContext(), "Configuration Saved Successfully ", Toast.LENGTH_SHORT).show();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        if (fos != null){
+                            try {
+                                fos.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    Log.d("onClick: ", "Data ==> " + new_ip);
+                    Log.d("onClick: ", "SSID ==> " + ssid);
+                    Log.d("onClick: ", "PASS ==> " + passwd);
+                    Log.d("onClick: ", "RES_ID ==> " + resource_id);
                 }
-
-
-                LF_Resource p = new LF_Resource(143, ip, "2");
-                p.start();
-                state = p.lfresource.get_state();
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        state = p.lfresource.get_state();
-                        if (state == STOPPED){
-                            Toast.makeText(v.getContext(), "Server STOPPED", Toast.LENGTH_LONG).show();
-                        }
-                        if (state == STARTING){
-                            Toast.makeText(v.getContext(), "Server is STARTING", Toast.LENGTH_LONG).show();
-                        }
-                        if (state == RUNNING){
-                            Toast.makeText(v.getContext(), "Server is RUNNING", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                }, 1000);
-
-                Handler handler1 = new Handler();
-                handler1.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        handler.removeCallbacks(this);
-                        state = p.lfresource.get_state();
-                        if (state == RUNNING){
-                            Toast.makeText(v.getContext(), "Connected to LANforge Server", Toast.LENGTH_LONG).show();
-                            openServerConnection();
-                        }
-                    }
-                }, 1000);
-
 
             }
         });
@@ -129,11 +144,48 @@ public class StartupActivity extends AppCompatActivity {
 //        Intent myIntent = new Intent(this, ServerConnection.class);
 //        startActivity(myIntent);
 
-    public void openServerConnection () {
+        public void openServerConnection () {
 
-        Intent myIntent = new Intent(this, navigation.class);
-        startActivity(myIntent);
+            Intent myIntent = new Intent(this, navigation.class);
+            startActivity(myIntent);
 
 
-    }
+        }
+
+        public void connect_server(String ip, String resource_id, View v){
+            LF_Resource p = new LF_Resource(143, ip, resource_id);
+            p.start();
+            state = p.lfresource.get_state();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    state = p.lfresource.get_state();
+                    if (state == STOPPED){
+                        Toast.makeText(v.getContext(), "Server STOPPED", Toast.LENGTH_LONG).show();
+                    }
+                    if (state == STARTING){
+                        Toast.makeText(v.getContext(), "Server is STARTING", Toast.LENGTH_LONG).show();
+                    }
+                    if (state == RUNNING){
+                        Toast.makeText(v.getContext(), "Server is RUNNING", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }, 1000);
+
+            Handler handler1 = new Handler();
+            handler1.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    handler.removeCallbacks(this);
+                    state = p.lfresource.get_state();
+                    if (state == RUNNING){
+                        Toast.makeText(v.getContext(), "Connected to LANforge Server", Toast.LENGTH_LONG).show();
+                        new_resource_id = p.getResource();
+                        openServerConnection();
+                    }
+                }
+            }, 1000);
+
+        }
 }
