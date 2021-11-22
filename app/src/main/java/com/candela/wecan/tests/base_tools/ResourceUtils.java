aecf13e19e0fb7e4f2a29d3037db00a875bb7eae
shivam <shivam.thakur@candelatech.com>
2021-11-23 04:29:06 +0530

    requestPlatformUpdate back again
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- app/src/main/java/com/candela/wecan/tests/base_tools/ResourceUtils.java
+++ app/src/main/java/com/candela/wecan/tests/base_tools/ResourceUtils.java
@@ -147,10 +147,62 @@ public class ResourceUtils extends AppCompatActivity implements AndroidUI{
 
         // TODO:  Fix me, PlatformInfo, Build and WifiManager objects provide useful info.
 
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        String product = Build.PRODUCT;
+        String username = Build.USER;
+        String release = Build.VERSION.RELEASE;
+        String version_incremental = Build.VERSION.INCREMENTAL;
+        int version_sdk_number = Build.VERSION.SDK_INT;
+        String board = Build.BOARD;
+        String brand = Build.BRAND;
+        String cpu_abi = Build.CPU_ABI;
+        String[] cpu_abi2 = (Build.SUPPORTED_ABIS);
+        String hardware = Build.HARDWARE;
+        String host = Build.HOST;
+        String id = Build.ID;
+        long availMem  = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long totalMem  = Runtime.getRuntime().totalMemory();
+        Vector<StringKeyVal> wifi_capabilities = new Vector<StringKeyVal>();
+        Vector<String> wifi_encryption ;
+
         pi.manufacturer = Build.MANUFACTURER;
         pi.model = Build.MODEL;
-        pi.wifi_capabilities = new Vector<>();
-        pi.username = "";
+        pi.product = Build.PRODUCT;
+        pi.username = Build.USER;
+        pi.release = Build.VERSION.RELEASE;
+        pi.version_incremental = Build.VERSION.INCREMENTAL;
+        pi.version_sdk_number = String.valueOf(Build.VERSION.SDK_INT);
+        pi.board = Build.BOARD;
+        pi.brand = Build.BRAND;
+        pi.cpu_abi = Build.CPU_ABI;
+        pi.cpu_abi2 = Build.CPU_ABI2;
+        pi.hardware = Build.HARDWARE;
+        pi.host = Build.HOST;
+        pi.id = Build.ID;
+        pi.availMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        pi.totalMem = Runtime.getRuntime().totalMemory();
+        List<ScanResult> scanResults = ((WifiManager) getSystemService(Context.WIFI_SERVICE)).getScanResults();
+//        pi.wifi_capabilities = ;
+//        pi.wifi_encryption = ;
+
+////        https://developer.android.com/reference/android/net/wifi/ScanResult#WIFI_STANDARD_11N
+
+
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        Boolean AC_11 = wifiManager.isWifiStandardSupported(ScanResult.WIFI_STANDARD_11AC);
+        Boolean AX_11 = wifiManager.isWifiStandardSupported(ScanResult.WIFI_STANDARD_11AX);
+        Boolean N_11 = wifiManager.isWifiStandardSupported(ScanResult.WIFI_STANDARD_11N);
+        Boolean legacy = wifiManager.isWifiStandardSupported(ScanResult.WIFI_STANDARD_LEGACY);
+
+        wifi_capabilities.add(new StringKeyVal("supports_5G", String.valueOf((wifiManager.is5GHzBandSupported()))));
+        wifi_capabilities.add(new StringKeyVal("supports_6G", String.valueOf((wifiManager.is6GHzBandSupported()))));
+        wifi_capabilities.add(new StringKeyVal("supports_2G", String.valueOf((wifiManager.is24GHzBandSupported())))); // This line gives an error
+        wifi_capabilities.add(new StringKeyVal("11-AC", String.valueOf(AC_11)));
+        wifi_capabilities.add(new StringKeyVal("11-AX", String.valueOf(AX_11)));
+        wifi_capabilities.add(new StringKeyVal("11-N", String.valueOf(N_11)));
+        wifi_capabilities.add(new StringKeyVal("LEGACY", String.valueOf(legacy)));
         return pi;
 
 
