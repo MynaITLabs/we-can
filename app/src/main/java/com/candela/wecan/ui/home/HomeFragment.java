983a6ff8eaaec496c4bf13e5aaa1ec30ebccee97
Amrit Raj <amrit.raj@candelatech.com>
2021-12-01 11:37:25 +0530

    Theme color changes
    Signed-off-by: Amrit Raj <amrit.raj@candelatech.com>

--- app/src/main/java/com/candela/wecan/ui/home/HomeFragment.java
+++ app/src/main/java/com/candela/wecan/ui/home/HomeFragment.java
@@ -109,7 +109,7 @@ public class HomeFragment extends Fragment {
                         wifi_info.put("SSID", wifiinfo.getSSID());
                         wifi_info.put("MAC", wifiinfo.getMacAddress());
                         wifi_info.put("BSSID", wifiinfo.getBSSID());
-                        wifi_info.put("State", String.valueOf(wifiinfo.getSupplicantState()));
+                        wifi_info.put("State",  String.valueOf(wifiinfo.getSupplicantState()));
 //                        wifi_info.put("Standard ", String.valueOf(wifiinfo.getWifiStandard())); // Getting error for few phones
                         wifi_info.put("LinkSpeed", String.valueOf(wifiinfo.getLinkSpeed()));
                         wifi_info.put("frequency", String.valueOf(wifiinfo.getFrequency()));
